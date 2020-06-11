373bce838cd15a6418b558e78fca17bb55e5ac9c
bensheridanedwards <bensheridanedwards@gmail.com>
2020-06-11 12:26:02 +0100

    Test(Feat): Check spelling of multple words against correct spellings

--- lib/spelling.rb
+++ lib/spelling.rb
@@ -1,9 +1,15 @@
 class Spelling
   def checker(string)
-    if string != "hello"
-      return "~#{string}~"
-    else
-      return string
+    word_array = string.split(' ')
+    
+    word_array.each_with_index do |word, index|
+      if ["hello", "there"].include?(word)
+        break
+      else
+        word_array[index] = "~#{word}~"
+      end
     end
+    
+    return word_array.join(' ')
   end
 end
--- spec/spelling_spec.rb
+++ spec/spelling_spec.rb
@@ -2,12 +2,20 @@ require 'spelling'
 
 describe Spelling do
   describe '.checker' do
-    it "takes in a word 'hello' that has the correct spelling and returns it" do
-      expect(subject.checker('hello')).to eq('hello')
+    context "when a single word is passed into the spell checker" do
+      it "takes in a word 'hello' that has the correct spelling and returns it" do
+        expect(subject.checker('hello')).to eq('hello')
+      end
+
+      it "takes in a word 'hellooo' that has been misspelt and returns it with tildas ~ ~ highlighting it" do
+        expect(subject.checker('hellooo')).to eq('~hellooo~')
+      end
     end
 
-    it "takes in a word 'hellooo' that has been misspelt and returns it with tildas ~ ~ highlighting it" do
-      expect(subject.checker('hellooo')).to eq('~hellooo~')
+    context "when two words are passed into the spell checker" do
+      it "takes in two words 'hello there' that has the correct spelling and returns them" do
+        expect(subject.checker('hello there')).to eq('hello there')
+      end
     end
   end
 end
